c091bfe549599c4f1feb04f9bf9ae4b9608ef1a3
Akarsh Srivastava <akrsh24@gmail.com>
2022-03-07 21:35:59 +0530

    Revert "Feature/typescript integration"

---
+++ db/schema/friendSchema.ts
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+export const friendSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    gender: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    language: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    contacts: {
+        type: Array
+    },
+});
--- dist/model/Series.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Rating;
-(function (Rating) {
-    Rating["ONE"] = "one";
-    Rating["TWO"] = "two";
-    Rating["THREE"] = "three";
-})(Rating || (Rating = {}));
